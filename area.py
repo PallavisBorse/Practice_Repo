08e1490347b212eb0e712d35f10ab702aa6211e9
Pallavi Borse <borsepallavi630@gmail.com>
2024-04-23 06:22:40 +0000

    code for python

---
+++ area.py
@@ -0,0 +1,10 @@
+# Define the dimensions of the rectangle
+length = 5
+width = 3
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the result
+print("The area of the rectangle is:", area)
+
